962c0a5daec7adfd24f309fddde953d19a587fba
Psycho <56837234+psychopattt@users.noreply.github.com>
2021-11-07 22:02:40 -0500

    Added button to cleanup descriptions, various enhancements, bug fixes and some cleanup

--- MiscTools/MiscToolsMainWindow.xaml.cs
+++ MiscTools/MiscToolsMainWindow.xaml.cs
@@ -5,12 +5,17 @@ using Playnite.SDK.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
+using System.Text;
 
 namespace MiscTools
 {
     public partial class MiscToolsMainWindow : PluginUserControl
     {
         private readonly string[] generatedTags = new string[] { "Missing Media", "Large Media" };
+        private readonly Regex aboutGameRegex = new Regex(@"<h1>About the Game<\/h1>([\s\S]+)", RegexOptions.Compiled);
+        private readonly Regex imgRegex = new Regex(@"(<[ap][^>]*>)?(<br>|<br\/>)?<img[^>]*>(<br>|<br\/>)?(<\/[ap]>)?", RegexOptions.Compiled);
+
         private static readonly ILogger logger = LogManager.GetLogger();
         private IPlayniteAPI PlayniteApi;
 
@@ -68,8 +73,10 @@ namespace MiscTools
 
             // Check if the tag already exists
             foreach (Tag tag in PlayniteApi.Database.Tags)
+            {
                 if (tag.Name == tagName)
                     tagGuid = tag.Id;
+            }
 
             return tagGuid;
         }
@@ -89,35 +96,96 @@ namespace MiscTools
             return tagGuid;
         }
 
-        private void AddGameTag(Game game, Guid newTagGuid)
+        // Returns true if the tag has been added
+        private bool AddGameTag(Game game, Guid newTagGuid)
         {
             if (game.TagIds == null) // No tags
             {
                 game.TagIds = new List<Guid> { newTagGuid };
                 PlayniteApi.Database.Games.Update(game);
+                return true;
             }
-            else if (!game.TagIds.Contains(newTagGuid)) // No "Missing Media" tag
+            else if (!game.TagIds.Contains(newTagGuid)) // Does not have the tag
             {
                 game.TagIds.Add(newTagGuid);
                 PlayniteApi.Database.Games.Update(game);
+                return true;
             }
+
+            return false; // Game already has tag
         }
 
         private void btnMissingMedia_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            uint updateCount = 0;
             Guid tagGuid = GetTagId("Missing Media");
 
             foreach (Game game in PlayniteApi.Database.Games)
             {
                 if (string.IsNullOrWhiteSpace(game.Icon) || string.IsNullOrWhiteSpace(game.CoverImage) || string.IsNullOrWhiteSpace(game.BackgroundImage))
                 {
-                    AddGameTag(game, tagGuid);
+                    if (AddGameTag(game, tagGuid))
+                        updateCount++;
                 }
             }
+
+            PlayniteApi.Dialogs.ShowMessage(string.Format("{0} game{1} been updated.", updateCount, updateCount != 1 ? "s have" : " has"), "Missing Media Tags");
+        }
+
+        private void btnCleanDesc_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            uint updateCount = 0;
+
+            foreach (Game game in PlayniteApi.Database.Games)
+            {
+                if (!string.IsNullOrWhiteSpace(game.Description))
+                {
+                    bool updated = false;
+                    string description = game.Description;
+
+                    Match aboutGameMatch = aboutGameRegex.Match(description);
+
+                    if (aboutGameMatch.Success)
+                    {
+                        description = aboutGameMatch.Groups[1].Value;
+                        updated = true;
+                    }
+
+                    Match imgMatch = imgRegex.Match(description);
+
+                    if (imgMatch.Success)
+                    {
+                        int startIndex = 0;
+                        StringBuilder descBuilder = new StringBuilder();
+
+                        while (imgMatch.Success)
+                        {
+                            int length = imgMatch.Index - startIndex;
+                            descBuilder.Append(description.Substring(startIndex, length)); // Add everything between the last match and the current match
+                            startIndex += length + imgMatch.Value.Length;
+                            imgMatch = imgMatch.NextMatch();
+                        }
+
+                        descBuilder.Append(description.Substring(startIndex)); // Add everything after the last match
+                        description = descBuilder.ToString();
+                        updated = true;
+                    }
+
+                    if (updated)
+                    {
+                        game.Description = description;
+                        PlayniteApi.Database.Games.Update(game);
+                        updateCount++;
+                    }
+                }
+            }
+
+            PlayniteApi.Dialogs.ShowMessage(string.Format("{0} game description{1} been updated.", updateCount, updateCount != 1 ? "s have" : " has"), "Description Cleanup");
         }
 
         private void btnLargeMedia_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            uint updateCount = 0;
             Guid tagGuid = GetTagId("Large Media");
 
             foreach (Game game in PlayniteApi.Database.Games)
@@ -128,19 +196,31 @@ namespace MiscTools
                 long size = Utilities.DirectorySize(new DirectoryInfo(gamePath)) / 1024;
 
                 if (size > maxSize)
-                    AddGameTag(game, tagGuid);
+                {
+                    if (AddGameTag(game, tagGuid))
+                        updateCount++;
+                }
             }
+
+            PlayniteApi.Dialogs.ShowMessage(string.Format("{0} game{1} been updated.", updateCount, updateCount != 1 ? "s have" : " has"), "Large Media Tags");
         }
 
         private void btnCleanTags_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            byte updateCount = 0;
+
             foreach (string tagName in generatedTags)
             {
                 Guid tagGuid = FindTagId(tagName);
                 
                 if (tagGuid != new Guid()) // Tag exists
+                {
                     PlayniteApi.Database.Tags.Remove(tagGuid);
+                    updateCount++;
+                }
             }
+
+            PlayniteApi.Dialogs.ShowMessage(string.Format("{0} tag{1} been deleted.", updateCount, updateCount != 1 ? "s have" : " has"), "Tag Cleaner");
         }
     }
 }
