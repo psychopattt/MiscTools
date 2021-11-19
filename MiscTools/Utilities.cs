6585f850b8eae7b07267f13dd09581a5a3c18f70
Psycho <56837234+psychopattt@users.noreply.github.com>
2021-11-19 13:01:44 -0500

    Added progress bar for descriptions cleanup

--- MiscTools/Utilities.cs
+++ MiscTools/Utilities.cs
@@ -2,6 +2,7 @@
 using Playnite.SDK.Models;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -9,6 +10,7 @@ namespace MiscTools
 {
     public static class Utilities
     {
+        private static readonly uint progressBarThreshold = 300;
         private static readonly Regex aboutGameRegex = new Regex(@"<h1>About the Game<\/h1>([\s\S]+)", RegexOptions.Compiled);
         private static readonly Regex imgRegex = new Regex(@"(<[ap][^>]*>)?(<br>|<br\/>)?<img[^>]*>(<br>|<br\/>)?(<\/[ap]>)?", RegexOptions.Compiled);
 
@@ -32,53 +34,96 @@ namespace MiscTools
 
         public static uint CleanDescriptions(IPlayniteAPI playniteApi, IEnumerable<Game> games)
         {
+            if (games.Count() < progressBarThreshold) // Don't show progress bar if there's less than X games to update
+                return CleanDescriptionsSilent(playniteApi, games);
+            else
+                return CleanDescriptionsProgress(playniteApi, games);
+        }
+
+        private static uint CleanDescriptionsSilent(IPlayniteAPI playniteApi, IEnumerable<Game> games)
+        {
             uint updateCount = 0;
 
             foreach (Game game in games)
             {
-                if (!string.IsNullOrWhiteSpace(game.Description))
-                {
-                    bool updated = false;
-                    string description = game.Description;
-
-                    Match aboutGameMatch = aboutGameRegex.Match(description);
-
-                    if (aboutGameMatch.Success)
-                    {
-                        description = aboutGameMatch.Groups[1].Value;
-                        updated = true;
-                    }
-
-                    Match imgMatch = imgRegex.Match(description);
-
-                    if (imgMatch.Success)
-                    {
-                        int startIndex = 0;
-                        StringBuilder descBuilder = new StringBuilder();
-
-                        while (imgMatch.Success)
-                        {
-                            int length = imgMatch.Index - startIndex;
-                            descBuilder.Append(description.Substring(startIndex, length)); // Add everything between the last match and the current match
-                            startIndex += length + imgMatch.Value.Length;
-                            imgMatch = imgMatch.NextMatch();
-                        }
-
-                        descBuilder.Append(description.Substring(startIndex)); // Add everything after the last match
-                        description = descBuilder.ToString();
-                        updated = true;
-                    }
-
-                    if (updated)
-                    {
-                        game.Description = description;
-                        playniteApi.Database.Games.Update(game);
-                        updateCount++;
-                    }
-                }
+                if (CleanDescription(playniteApi, game))
+                    updateCount++;
             }
 
             return updateCount;
+        }
+
+        private static uint CleanDescriptionsProgress(IPlayniteAPI playniteApi, IEnumerable<Game> games)
+        {
+            uint updateCount = 0;
+
+            GlobalProgressOptions globalProgressOptions = new GlobalProgressOptions("Misc Tools - Cleaning Descriptions", true);
+            globalProgressOptions.IsIndeterminate = false;
+
+            playniteApi.Dialogs.ActivateGlobalProgress((globalProgress) =>
+            {
+                globalProgress.ProgressMaxValue = games.Count();
+
+                foreach (Game game in games)
+                {
+                    if (CleanDescription(playniteApi, game))
+                        updateCount++;
+
+                    globalProgress.CurrentProgressValue++;
+
+                    if (globalProgress.CancelToken.IsCancellationRequested)
+                        break; // Stop cleaning games
+                }
+
+            }, globalProgressOptions);
+
+            return updateCount;
+        }
+
+        private static bool CleanDescription(IPlayniteAPI playniteApi, Game game)
+        {
+            if (!string.IsNullOrWhiteSpace(game.Description))
+            {
+                bool updated = false;
+                string description = game.Description;
+
+                Match aboutGameMatch = aboutGameRegex.Match(description);
+
+                if (aboutGameMatch.Success)
+                {
+                    description = aboutGameMatch.Groups[1].Value;
+                    updated = true;
+                }
+
+                Match imgMatch = imgRegex.Match(description);
+
+                if (imgMatch.Success)
+                {
+                    int startIndex = 0;
+                    StringBuilder descBuilder = new StringBuilder();
+
+                    while (imgMatch.Success)
+                    {
+                        int length = imgMatch.Index - startIndex;
+                        descBuilder.Append(description.Substring(startIndex, length)); // Add everything between the last match and the current match
+                        startIndex += length + imgMatch.Value.Length;
+                        imgMatch = imgMatch.NextMatch();
+                    }
+
+                    descBuilder.Append(description.Substring(startIndex)); // Add everything after the last match
+                    description = descBuilder.ToString();
+                    updated = true;
+                }
+
+                if (updated)
+                {
+                    game.Description = description;
+                    playniteApi.Database.Games.Update(game);
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
